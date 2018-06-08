5efb9fb4c912e4a6aabaf21e259874e115f834e5
Liliana Gutiérrrez <lilianaf.750@gmail.com>
2018-06-08 11:42:36 -0500

    Ejercicio 4. Lògica del seekbar y el switch

--- xamarinAndroidTaskApp/NewTaskFormActivity.cs
+++ xamarinAndroidTaskApp/NewTaskFormActivity.cs
@@ -20,6 +20,9 @@ namespace xamarinAndroidTaskApp
 
             SetContentView(Resource.Layout.activity_new_task_form);
 
+            Log.Debug(tag, "OnCreate() method");
+
+            //                                                       // To register the click for buttons.
             Button buttonCancel = FindViewById<Button>(Resource.Id.buttonCancel);
             buttonCancel.Click += (sender, args) =>
             {
@@ -32,10 +35,43 @@ namespace xamarinAndroidTaskApp
                 SaveNewTask(sender, args);
             };
 
+            TextView textPercentage = FindViewById<TextView>(Resource.Id.textPercentage);
+            string srtPercentage = "0%";
+            bool boolDone = false;
 
-            // Create your application here
+            SeekBar seekBarPercentage = FindViewById<SeekBar>(Resource.Id.seekBarPercentage);
+            seekBarPercentage.ProgressChanged += (object sender, SeekBar.ProgressChangedEventArgs e) =>
+            {
+                Log.Debug(tag, "como esta done " + boolDone);
+                if (boolDone)
+                {
+                    srtPercentage = string.Format("{0}%", e.Progress);
+                    textPercentage.Text = string.Format("100%");
+                }
+                else
+                {
+                    srtPercentage = string.Format("{0}%", e.Progress);
+                    textPercentage.Text = srtPercentage;
+                }
+            };
+
+            Switch switchDone = FindViewById<Switch>(Resource.Id.switchDone);
+            switchDone.CheckedChange += (object sender, CompoundButton.CheckedChangeEventArgs e) => 
+            {
+                if (e.IsChecked)
+                {
+                    boolDone = true;
+                    textPercentage.Text = string.Format("100%");
+                }
+                else{
+                    boolDone = false;
+                    textPercentage.Text = srtPercentage;
+                }
+            };
+
+
         }
-
+       
         //----------------------------------------------------------------------------------------------
         public void CancelNewTask(object sender, EventArgs e)
         {
